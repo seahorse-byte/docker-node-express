19ff33498beb3853d4af277756f15b7ea5eb6a5e
Olsi Gjeci <olsi.developer@gmail.com>
2022-07-04 22:03:47 +0100

    mongo

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://olsi:olsi@172.25.0.2:27017/?authSource=admin")
+  .then(() => {
+    console.log("Succeessfully connected to mongo database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 const port = process.env.PORT || 3000;
 
